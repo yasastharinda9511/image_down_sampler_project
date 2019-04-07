8bce64d596415d8d0ba345348f8e818bae4860f4
Yasas Tharinda <37435024+yasastharinda9511@users.noreply.github.com>
2019-04-07 11:35:23 +0530

    Update IRAM.v

--- IRAM.v
+++ IRAM.v
@@ -6,6 +6,7 @@ module IRAM(address,word_out,clk);
 	
 	initial
 		begin
+			// assembly code should be written in here according to the defined instruction set 
 		end
 	
 	assign word_out=memory[address];
