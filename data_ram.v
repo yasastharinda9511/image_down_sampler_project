33239360f6fbe823998580fc23397be47efaf97f
Yasas Tharinda <37435024+yasastharinda9511@users.noreply.github.com>
2019-05-13 00:22:16 +0530

    Update data_ram.v

--- data_ram.v
+++ data_ram.v
@@ -1,6 +1,8 @@
-module data_ram(clk,write_en,bus,data_in,data_out,output_enable,data_enable,chip_en,UB,LB,address,sram_address);
+module data_ram(clk,write_en_reg,bus,data_in_reg,data_out_reg,output_enable,data_enable,chip_en,UB,LB,address_reg,sram_address,data_in_uart,data_out_uart,address_uart,uart_en,write_en_uart);
 	input clk; // this code is for the sram controoler
-	input [1:0] write_en;
+	input [1:0] write_en_reg;
+	input [1:0] write_en_uart;
+	
 	output output_enable;
 	output data_enable;
 	output chip_en;
@@ -8,12 +10,20 @@ module data_ram(clk,write_en,bus,data_in,data_out,output_enable,data_enable,chip
 	output LB;
 	inout [15:0] bus;
 	//inout [15:0] bus;
-	input [15:0] data_in;
-	output [15:0]data_out;
+	input [15:0] data_in_reg;
+	output [15:0]data_out_reg;
+	input [19:0] address_reg;
 	
-	input [19:0] address;
+	input [15:0] data_in_uart;
+	output [15:0]data_out_uart;
+	input [19:0] address_uart;
+	input uart_en;
+	
 	output [19:0] sram_address;
 	
+	reg [15:0] data_in;
+	
+	//output [7:0] rx_check;
 	//output [15:0]data_out;
 	
 	/*always@(posedge clk)
@@ -45,16 +55,30 @@ module data_ram(clk,write_en,bus,data_in,data_out,output_enable,data_enable,chip
 					//bus=16'bzzzzzzzzzzzzzzzz;
 				end*/
 		//end
+		always @(*)
+			begin
+				if(uart_en)
+					data_in= data_in_uart;
+				else
+					data_in= data_in_reg;
+			end
+			
+			
+		assign data_enable=(write_en_reg==2'b11 || write_en_uart== 2'b11) ? 1'b0 :1'b1 ;
+		assign chip_en=(write_en_reg==2'b11 || write_en_reg==2'b00 || write_en_uart==2'b11 || write_en_uart==2'b00) ? 1'b0 :1'b1 ;
+		assign output_enable=(write_en_reg==2'b00 || write_en_uart== 2'b00) ? 1'b0 :1'b1 ;
+		assign LB= (write_en_reg==2'b11 || write_en_reg==2'b00 || write_en_uart==2'b11 || write_en_uart==2'b00) ? 1'b0 :1'b1 ;
+		assign UB= (write_en_reg==2'b11 || write_en_reg==2'b00 || write_en_uart==2'b11 || write_en_uart==2'b00) ? 1'b0 :1'b1 ;
+	
+		assign data_out_reg=(write_en_reg ==2'b00 ) ? bus : 16'dz;
+		assign data_out_uart=(write_en_uart ==2'b00 ) ? bus : 16'dz;
 		
-		assign data_enable=(write_en==2'b11) ? 1'b0 :1'b1 ;
-		assign chip_en=(write_en==2'b11 || write_en==2'b00) ? 1'b0 :1'b1 ;
-		assign output_enable=(write_en==2'b00) ? 1'b0 :1'b1 ;
-		assign LB= (write_en==2'b11 || write_en==2'b00) ? 1'b0 :1'b1 ;
-		assign UB= (write_en==2'b11 || write_en==2'b00) ? 1'b0 :1'b1 ;
+		//assign data_in = (uart_en) ? data_in_uart : data_in_reg;
+		assign bus= (write_en_reg ==2'b11 || write_en_uart ==2'b11) ? data_in: 16'dz;
+		
+		assign sram_address= (uart_en) ? address_uart:address_reg;
 	
-		assign data_out=(write_en ==2'b00) ? bus : 16'dz;
-		assign bus= (write_en ==2'b11) ? data_in: 16'dz;
-		assign sram_address=address;
-	
+		
 	
 endmodule
+
