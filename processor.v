c9b31a930d17455abd2f9eb5d6e5f891fbde2d78
Yasas Tharinda <37435024+yasastharinda9511@users.noreply.github.com>
2019-05-13 21:16:13 +0530

    Update processor.v

--- processor.v
+++ processor.v
@@ -1,9 +1,11 @@
-module processor(clk,bus,sram_address,output_enable,data_enable,chip_en,UB,LB,check_a);
+module processor(clk,bus,sram_address,output_enable,data_enable,chip_en,UB,LB,check_a,rx,tx,rx_check);
 	input clk;
 	inout [15:0] bus;
 	output [19:0] sram_address;
 	output output_enable,data_enable,chip_en,UB,LB;
 	output [15:0]check_a;
+	input rx;
+	output tx;
 	
 	//data path intialization
 	wire[3:0] sel_d;
@@ -12,13 +14,16 @@ module processor(clk,bus,sram_address,output_enable,data_enable,chip_en,UB,LB,ch
 	wire [15:0] constant;
 	wire incr_en, merge_en, sel_c, ir_en;
 	wire [1:0] d_RAM_en;
-	wire zero, great;
+	wire zero, great,start_calculation;
 	wire [15:0] out_statemachine;
 	wire mem_write_en;
+	wire finish;
+	wire sample_clock;
 	
-	reg [39:0] counter=40'd0;
-	reg sample_clock=1'd0;
+	//reg [39:0] counter=40'd0;
+	//reg sample_clock=1'd0;
 
+	output [7:0] rx_check;
 	//data_path(sel_d, ctrl, flagA, flagB,constant,clk, incr_en, merge_en, sel_c, d_RAM_en,ir_en,zero, great,out_statemachine,bus,sram_address,output_enable,data_enable,chip_en,UB,LB)
 	
 	
@@ -44,7 +49,12 @@ module processor(clk,bus,sram_address,output_enable,data_enable,chip_en,UB,LB,ch
 	.UB(UB),
 	.LB(LB),
 	.check_a(check_a),
-	.mem_write_en(mem_write_en));
+	.mem_write_en(mem_write_en),
+	.start_calculation(start_calculation),
+	.finish(finish),
+	.tx(tx),
+	.rx(rx),
+	.rx_check(rx_check));
 //state_machine(clk,sel_d,ctrl,flagA,flagB,constant,incr_en, merge_en, sel_c, d_RAM_en,ir_en,zero,great,instruction);
 
 
@@ -62,21 +72,24 @@ module processor(clk,bus,sram_address,output_enable,data_enable,chip_en,UB,LB,ch
 	.zero(zero),
 	.great(great),
 	.instruction(out_statemachine),
-	.mem_write_en(mem_write_en));
+	.mem_write_en(mem_write_en),
+	.finish(finish),
+	.start_calculation(start_calculation));
 	
+	clock_devide(.clk(clk),
+	.sample_clock(sample_clock));
 	
-	always@(posedge clk)
-	begin
-		if (counter==40'd10000000)
-			begin
-				counter=40'd0;
-				sample_clock=1'b1;
-			end
-		else
-			begin
-				counter=counter +40'd1;
-				sample_clock=1'b0;
-			end
-	end
-	
-endmodule 
+	/*always@(posedge clk)
+		begin
+			if (counter==40'd9)
+				begin
+					counter=40'd0;
+					sample_clock=1'b1;
+				end
+			else
+				begin
+					counter=counter +40'd1;
+					sample_clock=1'b0;
+				end
+		end*/
+endmodule  
