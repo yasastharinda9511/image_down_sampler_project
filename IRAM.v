f3c8ea010ab0995dd33e36834848cf6574603b4f
Yasas Tharinda <37435024+yasastharinda9511@users.noreply.github.com>
2019-05-17 12:30:02 +0530

    Update IRAM.v

--- IRAM.v
+++ IRAM.v
@@ -32,7 +32,7 @@ module IRAM(address,word_out,clk);
 			memory[9]=16'd0;
 			memory[10]={MERGE,DCWR};
 			memory[11]={ADD,R1,R3,C_DR};
-		   memory[12]={MEM_WRITE,DCWR};
+		   	memory[12]={MEM_WRITE,DCWR};
 			memory[13]={CONSTANT2REG,C_DR,DCC};
 			memory[14]=16'd0;
 			memory[15]={CONSTANT2REG,C_R1,DCC};
@@ -87,8 +87,8 @@ module IRAM(address,word_out,clk);
 			memory[37]={OVER,DCWR};*/
 			
 						/*			KERNAL=		  | 1| 2| 1|			MEMORY INDEXING VALUES FOR R3	=		  |(0,0): -257|(0,1):-256|(0,2):-255|
-									       (1/16)*| 2| 4| 2|							                 			  |(1,0): -1  |(1,1):0   |(1,2):+1  |
-													  | 1| 2| 1|							                 			  |(2,0):+255 |(2,1):+256|(2,2):+257|
+									       	   (1/16)*| 2| 4| 2|							                  |(1,0): -1  |(1,1):0   |(1,2):+1  |
+											  | 1| 2| 1|							                  |(2,0):+255 |(2,1):+256|(2,2):+257|
 									       						*/
 
 			
@@ -621,11 +621,11 @@ endmodule
 
 
 /*				a_flag	/b_flag												  c_decorder
- 				3'd1 : A_bus = PC;										     PC= 4'd1
+ 				3'd1 : A_bus = PC;										     	  PC= 4'd1
 				3'd2 : A_bus = DR;											  DR= 4'd2
 				3'd3 : A_bus = R1;											  R1= 4'd3
 				3'd4 : A_bus = R2;											  R2= 4'd4
 				3'd5 : A_bus = R3;											  R3= 4'd5
 				3'd6 : A_bus = R4;											  R4= 4'd6
-				3'd7 : A_bus = R5;                                   R5= 4'd7
+				3'd7 : A_bus = R5;                                   							  R5= 4'd7
 																					  TR= 4'd8 */
